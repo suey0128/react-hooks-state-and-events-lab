55817a4b6f9cf5d04c3db823521515df6fe8c80a
suey0128 <syui0128@gmail.com>
2021-06-09 17:11:51 -0700

    commit tree 4134f04b552873d855ea535687d3eb9714cdabb6

--- src/components/App.js
+++ src/components/App.js
@@ -4,15 +4,20 @@ import itemData from "../data/items";
 
 function App() {
   const [items, setItems] = useState(itemData);
+  const [isDark, setIsDark] = useState(false)
+
+  const btnHandler = (e) => {
+    setIsDark(!isDark)
+  }
 
   // this data will be passed down to the ShoppingList as a prop
-  console.log(items);
+  console.log('items:',items);
 
   return (
-    <div className={"App " + (false ? "dark" : "light")}>
+    <div className={"App " + (isDark ? "dark" : "light")}>
       <header>
         <h2>Shopster</h2>
-        <button>Dark Mode</button>
+        <button onClick={e=>{btnHandler(e)}}>Dark Mode</button>
       </header>
       <ShoppingList items={items} />
     </div>
